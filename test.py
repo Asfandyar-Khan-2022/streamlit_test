1a9193bb86bd86aa1dee767a31f82ed46e49876d
Asfandyar Khan <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2024-03-15 08:07:26 +0000

    Update test.py

--- test.py
+++ test.py
@@ -14,10 +14,11 @@ def get_details(poke_number):
 		pokemon = response.json()
 		return (pokemon['name'], pokemon['height'], pokemon['weight'], len(pokemon['moves']),
 		                pokemon['sprites']['other']['official-artwork']['front_default'], 
-						pokemon['cries']['latest'])
+						pokemon['cries']['latest'],
+						pokemon['types'][0]['type']['name'])
 	except:
 		return 'Error', np.NAN, np.NAN, np.NAN
-	
+
 
 pokemon_number = st.slider("Pick a pokemon",
 						   min_value=1,
@@ -26,24 +27,43 @@ pokemon_number = st.slider("Pick a pokemon",
 
 
 
-
-d = {'name': [get_details(pokemon_number)[0]], 'height': [get_details(pokemon_number)[1]], 'weight': [get_details(pokemon_number)[2]], 'moves': [get_details(pokemon_number)[3]]}
+d = {'name': [get_details(pokemon_number)[0]], 'height': [get_details(pokemon_number)[1]], 'weight': [get_details(pokemon_number)[2]], 'moves': [get_details(pokemon_number)[3]], 'type': [get_details(pokemon_number)[6]]}
 
 df = pd.DataFrame(d)
 
 print(df)
 
+name, height, weight, moves, image, cries, type = get_details(pokemon_number)
 
+pokemon_colours = {
+    "normal": "#A8A77A",
+    "fire": "#EE8130",
+    "water": "#6390F0",
+    "electric": "#F7D02C",
+    "grass": "#7AC74C",
+    "ice": "#96D9D6",
+    "fighting": "#C22E28",
+    "poison": "#A33EA1",
+    "ground": "#E2BF65",
+    "flying": "#A98FF3",
+    "psychic": "#F95587",
+    "bug": "#A6B91A",
+    "rock": "#B6A136",
+    "ghost": "#735797",
+    "dragon": "#6F35FC",
+    "dark": "#705746",
+    "steel": "#B7B7CE",
+    "fairy": "#D685AD",
+    "stellar": "#FFD700"
+}
 
-name, height, weight, moves, image, cries = get_details(pokemon_number)
+print(pokemon_colours[type])
 
-# df = pd.DataFrame(name, height)
+df_height = pd.DataFrame({'Height': [5, height, 7], 'Column': ['Growlithe', name, 'Bulbasaur']})
+df_height.set_index('Column', inplace=True)
+df_height.index.name = 'Name'
 
-# st.write(f'Name: {name}')
-# st.write(f'Height: {height}')
-# st.write(f'Weight: {weight}')
-# st.write(f'Move Count: {moves}')
 st.table(df)
 st.audio(cries)
 st.image(image)
-
+st.bar_chart(df_height, color=pokemon_colours[type])
